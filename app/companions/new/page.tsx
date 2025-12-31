b75eaa3e13200300c8ce8ed61c0bcb12ca37872f
unknown <mahmadcoder>
2025-12-31 21:35:28 +0500

    Implemented Billing, Database Setup and Create Companion Server action

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,15 +1,19 @@
-import CompanionForm from "@/components/CompanionForm"
+import CompanionForm from "@/components/CompanionForm";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
+const NewCompanion = async () => {
+  const { userId } = await auth();
+  if (!userId) redirect("/sign-in");
 
-const NewCompanion = () => {
   return (
     <main className="lg:w-1/3 md:w-2/3 items-center justify-center">
       <article className="w-full gap-4 flex flex-col">
         <h1>Companion Builder</h1>
-        <CompanionForm/>
+        <CompanionForm />
       </article>
     </main>
-  )
-}
+  );
+};
 
-export default NewCompanion
+export default NewCompanion;
